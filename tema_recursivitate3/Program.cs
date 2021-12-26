f53cf35cf0271143e562a95cd18b0bdf6af8a31d
Iosif Turcu <iosif.turcu@emmanuel.ro>
2021-12-26 09:45:05 +0200

    Running

--- tema_recursivitate3/Program.cs
+++ tema_recursivitate3/Program.cs
@@ -6,7 +6,25 @@ namespace tema_recursivitate3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter the index of the sequence: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Fibonacci of {i} = " + GetFibonacciSequence(i));
+            }
+        }
+
+        public static int GetFibonacciSequence(int n)
+        {
+            if(n == 0)
+            {
+                return 0;
+            }
+            if(n == 1)
+            {
+                return 1;
+            }
+            return (GetFibonacciSequence(n - 1) + GetFibonacciSequence(n - 2));
         }
     }
 }
