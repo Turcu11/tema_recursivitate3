f0852d37e56ab207bebb790867159a12a4f0b174
Iosif Turcu <iosif.turcu@emmanuel.ro>
2021-12-26 09:24:21 +0200

    intial setup

---
+++ tema_recursivitate3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tema_recursivitate3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
